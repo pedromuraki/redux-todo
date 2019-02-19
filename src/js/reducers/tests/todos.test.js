e95400bf2058198e6239e79f2c711b17dd90dd16
Pedro Muraki <pedromuraki@gmail.com>
2019-02-19 16:57:15 -0300

    Refactored todos reducer - Changed isDone prop to status

--- src/js/reducers/tests/todos.test.js
+++ src/js/reducers/tests/todos.test.js
@@ -11,16 +11,16 @@ describe('ADD_ITEM', () => {
   it('should add a new item', () => {
     const initialState = deepFreeze([]);
     const action = deepFreeze({ type: 'ADD_ITEM', content: 'Example item' });
-    const endState = [{ content: 'Example item', isDone: false }];
+    const endState = [{ content: 'Example item', status: 'to-do' }];
     expect(todos(initialState, action)).to.be.eql(endState);
   });
 
   it('should add a new item again', () => {
-    const initialState = deepFreeze([{ content: 'Example item', isDone: false }]);
+    const initialState = deepFreeze([{ content: 'Example item', status: 'to-do' }]);
     const action = deepFreeze({ type: 'ADD_ITEM', content: 'Another example item' });
     const endState = [
-      { content: 'Example item', isDone: false },
-      { content: 'Another example item', isDone: false }
+      { content: 'Example item', status: 'to-do' },
+      { content: 'Another example item', status: 'to-do' }
     ];
     expect(todos(initialState, action)).to.be.eql(endState);
   });
@@ -29,26 +29,26 @@ describe('ADD_ITEM', () => {
 describe('REMOVE_ITEM', () => {
   it('should remove an item based on its index', () => {
     const initialState = deepFreeze([
-      { content: 'Example item', isDone: false },
-      { content: 'Another example item', isDone: false }
+      { content: 'Example item', status: 'to-do' },
+      { content: 'Another example item', status: 'to-do' }
     ]);
     const action = deepFreeze({ type: 'REMOVE_ITEM', index: 1 });
-    const endState = [{ content: 'Example item', isDone: false }];
+    const endState = [{ content: 'Example item', status: 'to-do' }];
     expect(todos(initialState, action)).to.be.eql(endState);
   });
 
   it('should remove another item based on its index', () => {
     const initialState = deepFreeze([
-      { content: 'Example item', isDone: false },
-      { content: 'Another example item', isDone: false },
-      { content: 'Another example item 2', isDone: false },
-      { content: 'Another example item 3', isDone: false }
+      { content: 'Example item', status: 'to-do' },
+      { content: 'Another example item', status: 'to-do' },
+      { content: 'Another example item 2', status: 'to-do' },
+      { content: 'Another example item 3', status: 'to-do' }
     ]);
     const action = deepFreeze({ type: 'REMOVE_ITEM', index: 2 });
     const endState = [
-      { content: 'Example item', isDone: false },
-      { content: 'Another example item', isDone: false },
-      { content: 'Another example item 3', isDone: false }
+      { content: 'Example item', status: 'to-do' },
+      { content: 'Another example item', status: 'to-do' },
+      { content: 'Another example item 3', status: 'to-do' }
     ];
     expect(todos(initialState, action)).to.be.eql(endState);
   });
@@ -57,30 +57,30 @@ describe('REMOVE_ITEM', () => {
 describe('CHECK_ITEM', () => {
   it('should mark item as done based on its index', () => {
     const initialState = deepFreeze([
-      { content: 'Example item', isDone: false },
-      { content: 'Another example item', isDone: false }
+      { content: 'Example item', status: 'to-do' },
+      { content: 'Another example item', status: 'to-do' }
     ]);
     const action = deepFreeze({ type: 'CHECK_ITEM', index: 1 });
     const endState = [
-      { content: 'Example item', isDone: false },
-      { content: 'Another example item', isDone: true }
+      { content: 'Example item', status: 'to-do' },
+      { content: 'Another example item', status: 'is-done' }
     ];
     expect(todos(initialState, action)).to.be.eql(endState);
   });
 
   it('should mark another item as done based on its index', () => {
     const initialState = deepFreeze([
-      { content: 'Example item', isDone: false },
-      { content: 'Another example item', isDone: false },
-      { content: 'Another example item 2', isDone: false },
-      { content: 'Another example item 3', isDone: true }
+      { content: 'Example item', status: 'to-do' },
+      { content: 'Another example item', status: 'to-do' },
+      { content: 'Another example item 2', status: 'to-do' },
+      { content: 'Another example item 3', status: 'is-done' }
     ]);
     const action = deepFreeze({ type: 'CHECK_ITEM', index: 0 });
     const endState = [
-      { content: 'Example item', isDone: true },
-      { content: 'Another example item', isDone: false },
-      { content: 'Another example item 2', isDone: false },
-      { content: 'Another example item 3', isDone: true }
+      { content: 'Example item', status: 'is-done' },
+      { content: 'Another example item', status: 'to-do' },
+      { content: 'Another example item 2', status: 'to-do' },
+      { content: 'Another example item 3', status: 'is-done' }
     ];
     expect(todos(initialState, action)).to.be.eql(endState);
   });
--- src/js/reducers/todos.js
+++ src/js/reducers/todos.js
@@ -3,14 +3,14 @@ const todos = (state = [], action) => {
     case 'ADD_ITEM':
       return state.concat({
         content: action.content,
-        isDone: false
+        status: 'to-do'
       });
 
     case 'REMOVE_ITEM':
       return state.filter((_, i) => i !== action.index);
 
     case 'CHECK_ITEM':
-      return state.map((item, i) => i === action.index ? { ...item, isDone: true } : item);
+      return state.map((item, i) => i === action.index ? { ...item, status: 'is-done' } : item);
   }
   return state;
 }
@@ -18,9 +18,10 @@ const todos = (state = [], action) => {
 export default todos;
 
 // const renderItems = (filter) => {
-//   if (!filter) return todos.map((item, i) => <Item key={} index={i} />);
+//   if (!filter) return todos.map((item, i) => <Item index={i} />);
 
 //   return todos.map((item, i) => {
-//     if
-//   })
+//     if (item.status === filter) return <Item index={i} />
+//     return;
+//   });
 // }
