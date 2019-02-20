d134bf734ef079ec3b8916df66561a95bf531f9d
Pedro Muraki <pedromuraki@gmail.com>
2019-02-19 22:04:07 -0300

    Finished Form component with addItem action configured

--- src/js/components/Form.js
+++ src/js/components/Form.js
@@ -1,22 +1,46 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
 
-// const mapDispatchToProps = dispatch => {
-//   return {
-//     addCounter: () => dispatch(addCounter())
-//   };
-// };
+import addItem from '../actions/todos/addItem';
+
+const mapDispatchToProps = dispatch => {
+  return {
+    addItem: (content) => dispatch(addItem(content))
+  };
+};
 
 class Form extends Component {
+  constructor() {
+    super();
+
+    this.state = {
+      inputValue: ''
+    };
+
+    this.handleChange = (e) => {
+      this.setState({ inputValue: e.target.value });
+    }
+
+    this.handleSubmit = (e) => {
+      e.preventDefault();
+      this.props.addItem(this.state.inputValue);
+      this.setState({ inputValue: '' });
+    }
+  }
+
   render() {
     return (
-      <form>
+      <form onSubmit={this.handleSubmit}>
         <h3>Add new item:</h3>
-        <input type="text" /> <button type="submit">Add</button>
+        <input
+          type="text"
+          onChange={this.handleChange}
+          value={this.state.inputValue}
+          required
+        /> <button type="submit">Add</button>
       </form>
     )
   }
 }
 
-// export default connect(null, mapDispatchToProps)(Form);
-export default Form;
+export default connect(null, mapDispatchToProps)(Form);
